5de2de7d4089a6760b09f9194ff48ec2aa9cfbd9
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-25 22:33:47 -0400

    Working on algorythm problems

---
+++ app.js
@@ -0,0 +1,14 @@
+//quadratic-take the first letter, if it appears after running indexof.then we move on to the next letter. repeat.
+//string with 1,000,000
+//for each to all million takes forever
+
+
+
+
+//linear = 1,000,000 + 1,000,000 + 1,000,000 +1,000,000
+
+//first step is to build an object from each letter in string
+// ask a question as it being built is this key already here? if so, set the value to fale. If not, set the value as a tuple in the tuple = [index, true];
+
+
+//for loop 
