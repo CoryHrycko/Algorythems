c62414d64c8eccc501130cf4e7fb135c82f44f9c
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-25 22:45:47 -0400

    Working on algorythm problems

--- app.js
+++ app.js
@@ -11,4 +11,40 @@
 // ask a question as it being built is this key already here? if so, set the value to fale. If not, set the value as a tuple in the tuple = [index, true];
 
 
-//for loop 
+//for loop if false move on
+//if != false, answer is letter if index<lowestIndex,
+
+//return answer || default
+
+
+function noRepeat(str){
+    let arr = str.split('');
+    let obj = {};
+    let answer;
+    let minIndex = arr.length;
+
+    arr.forEach((letter, index) => {
+        if (!obj[letter]) {
+            obj[letter]=false;
+        }
+        
+        else if(obj[letter]){
+            obj[letter]=false;
+        }
+    });
+
+    for ( let key in obj){
+        if (!obj[key]){
+
+        }
+
+        else {
+            if
+        (obj[key][0]<minIndex){
+            answer = key;
+            minIndex = obj[key][0]
+        }   
+        }
+    }
+    return answer
+}
