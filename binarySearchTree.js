f402112237d944e759d5f69938c671f8268e0888
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-30 22:56:38 -0400

    uploading progress on problem solving algys

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -74,44 +74,75 @@ class BST {
     }
     isPresent(data){
         let current =this.root;
+        while(current){
+            if(data ===current.data){
+                return true;
+            }
+            if(data<current.data){
+                current=current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    remove(data){
+        const removeNode = function(node,data){
+            if(node=null){
+                return null;
+            }
+            if(data == node.data){
+            //no children
+                if(node.left == null && node.right ==null){
+                    return null;
+                }
+                //no left children
+                if(node.left ==null){
+                    return node.right;
+                }
+                //no right 
+                if(node.right == null){
+                    return node.left;
+                }
+                // node has both
+                let tempNode = node.right;
+                while(tempNode.left !==null){
+                    tempNode =tempNode.left;
+                }
+                node.data = tempNode.data;
+                node.right=removeNode(node.right,tempNode.data);
+                return nodel
+            }else if(data <node.data){
+                node.left = removeNode(node.left, data);
+                return node;
+            }else {
+                node.right = removeNode(node.right, data);
+                return node
+            }
+        }
+        this.root = removeNode(this.root, data);
+    }
 }
 
-
+BST.add(33);
+BST.add(7);
+BST.add(6);
+BST.add(454);
+BST.add(22);
+BST.add(90);
+BST.add(12);
+BST.add(20);
+BST.add(30);
+BST.add(543);
+BST.add(400);
+BST.add(465);
+BST.add(4);
+BST.add(3);
+BST.remove(4);
+console.log(BST.findMin());
+console.log(BST.findMax());
+BST.remove(7);
+console.log(BST.isPresent(4));
 
 
 
