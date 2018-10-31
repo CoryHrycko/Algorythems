159340d354ed87ef116855b9fda9a429d4ec3de0
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-30 20:10:21 -0400

    uploading progress on a linked list algy

--- stackDataStructures.js
+++ stackDataStructures.js
@@ -70,6 +70,13 @@ let myStack = new Stack();
 
 myStack.push(1);
 myStack.push(2);
-console.log(myStack.peek);
-console.log(myStack.pop);
-console.log(myStack.peek);
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+myStack.push('words');
+console.log(myStack.size());
+console.log(myStack.peek());
+console.log(myStack.pop());
+console.log(myStack.peek());
+
+
