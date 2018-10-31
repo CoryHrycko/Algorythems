cd328682c48eb7f0ad668f48f0fe27bccfa93020
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-10-30 22:26:43 -0400

    uploading progress on problem solving algys

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -26,6 +26,7 @@ class BST {
                 if(data<node.data){
                     if(node.left === null){
                         node.left = new Node(data);
+                        return;
                     }else if(node.left !==null){
                      return searchTree(node.left);
                     }
@@ -40,16 +41,75 @@ class BST {
                     return null;
                 }
             };
-            //return searchTree(node);
+            return searchTree(node);
         }
-        return searchTree(node);
     }
+    findMin(){
+        let current = this.root;
+        while(current.left !==null){
+            current = current.left;
+        }
+        return current.data;
+    };
+    findMax(){
+        let current = this.root;
+        while(current.right !==null){
+            current = current.right;
+        }
+        return current.data;
+    };
+    find(data){
+        let current = this.root;
+        while(current.data !==data){
+            if(data < current.data){
+                current = current.left;
+            }else {
+                current = current.right;
+            }
+            if(current ===null){
+                return null;
+            }
+        }
+        return current;
+    }
+    isPresent(data){
+        let current =this.root;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
 
 
 
